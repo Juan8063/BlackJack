9f392c65887e4f6e2702559332e57a5bd45dc114
Juan8063 <197652802+Juan8063@users.noreply.github.com>
2025-03-31 17:43:24 +0000

    message

--- BlackJack.js
+++ BlackJack.js
@@ -1,13 +1,19 @@
-let dealerCards = 0;
+let dealerCards1 = 0;
+let dealerCards2 = 0;
+let dealerCardsTotal = dealerCards1 + dealerCards2; 
 let playerCards = 0;
+
 function getCards() {
      return Math.floor(Math.random()*12);
 }
 
 function startGame() {
-    dealerCards = getCards() + getCards();
-    playercards = getCards() + getCards();
+    dealerCards1 = getCards();
+    dealerCards2 = getCards();
+    playerCards = getCards() + getCards();
+    dealerCardsTotal = dealerCards1 + dealerCards2;
     console.log("you have a total of " + playerCards)
+    console.log ("dealer has a a total of " + dealerCardsTotal)
 }
 
 startGame();
